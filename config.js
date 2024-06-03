da53d668dbceef1969e6752c7903bb8cd8046f01
12kio <bouy4535@gmail.com>
2024-06-03 18:45:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     app: {
-        px: 'ton prefix',
-        token: 'ton token',
-        owners: 'id bruyer',
-        funny: 'id dev',
-        color: 'couleur',
+        px: '+',
+        token: 'MTE5OTA4MDQ2OTYyNzY2NjU1Mg.GN7d0Q.7p9I50ypyBaR_IuN4Ul9xgolh46tdidQ4IxzLc',
+        owners: '1174667044189384774',
+        funny: '1174667044189384774',
+        color: '0ms',
         footer: 'ton footer',
         maxserver: '6',
         maxVol: '150',
